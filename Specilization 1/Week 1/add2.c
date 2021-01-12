a46c6c37fdb65665ca3816b89ec6c7aec97d28f1
omicorn-byte <70059467+omicorn-byte@users.noreply.github.com>
2021-01-13 00:45:29 +0530

    '''' First commit in c language ''''

---
+++ Specilization 1/Week 1/add2.c
@@ -0,0 +1,15 @@
+/* Ready in two floats and print sum 
+    kumar
+    jan , 12 ,2021 . 11.35pm
+*/
+#include <stdio.h>
+int main(void)
+{
+	float a , b , sum ;
+	printf("Input two floats");
+	scanf("%f%f",&a,&b);
+	printf("a= %f , b = %f \n" , a, b);
+	sum = a+b;
+	printf("sum = %f\n\n", sum);
+	return 0;
+}
